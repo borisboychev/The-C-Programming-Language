15143b8b58fd5d56b5d7c8546517bd8f3e15e9a7
BorisBBoychev <borisboychev007@Gmail.com>
2020-08-18 18:10:43 +0300

    counting newlines blanks and tabs exercise1.8

---
+++ Chapter01/Exercise1.8/counting_blanks_tabs_newlines.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int c;
+    int tabs, blanks, newlines;
+    tabs = blanks = newlines = 0;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\t')
+        {
+            ++tabs;
+        }
+        else if (c == ' ')
+        {
+            ++blanks;
+        }
+        else if (c == '\n')
+        {
+            ++newlines;
+        }
+    }
+
+    printf("Blanks: %d | Tabs: %d | NewLines: %d", blanks, tabs, newlines);
+}
+
+//Exercise 1.8 from page 21
