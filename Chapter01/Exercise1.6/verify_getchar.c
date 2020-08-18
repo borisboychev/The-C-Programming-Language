189637ec3f739537124283218e8c74498ae1e83c
BorisBBoychev <borisboychev007@Gmail.com>
2020-08-18 13:15:25 +0300

    verifying getchar() exercise 1.6

---
+++ Chapter01/Exercise1.6/verify_getchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+    int c = getchar();
+    printf("getchar returns: %d", c != EOF);
+}
+
+//Exercise 1.6 from page 18
+//expression getchar() returns 1 if it is != EOF and 0 is it is == EOF
