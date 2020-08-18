ffddc5e91fbfe24cd7ea2dd075b294a86701f9c2
BorisBBoychev <borisboychev007@Gmail.com>
2020-08-18 12:46:12 +0300

    exercise 1.4 celsius to fahr

---
+++ Chapter01/Exercise1.4/fahr_to_celsius.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("---Celsius To Fahrenheit---\n");
+    float fahr, celsius;
+    float lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+
+    while (fahr <= upper)
+    {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("\t%.2f\t\t %.2f\n", celsius, fahr);
+        fahr = fahr + step;
+    }
+}
+
+//Exercise 1.4 from page 13
