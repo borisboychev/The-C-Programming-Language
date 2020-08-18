328a73a56ed643d543290acb1733b1034bc88e3b
BorisBBoychev <borisboychev007@Gmail.com>
2020-08-18 12:59:30 +0300

    reverse fahr to celsius exercise 1.5

--- Chapter01/Exercise1.5/reverse_fahr_to_celsius.c
+++ Chapter01/Exercise1.5/reverse_fahr_to_celsius.c
@@ -6,6 +6,8 @@ int main()
     printf("Fahr <----> Celsius\n");
     for (fahr = 300; fahr >= 0; fahr -= 20)
         printf("%.2f\t\t%.2f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+
+    return 0;
 }
 
 //Exercise 1.5 from page 15
