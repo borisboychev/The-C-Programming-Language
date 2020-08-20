d2b880fe6a237b18811c5c9c4f3a507f6edd54c4
BorisBBoychev <borisboychev007@Gmail.com>
2020-08-20 16:38:16 +0300

    exercise 1.11 test for bugs

---
+++ Chapter01/Exercise1.11/test.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define IN 1  //inside a word
+#define OUT 0 //outside a word
+
+int main()
+{
+    int c, nl, nw, nc, state;
+    state = OUT;
+    nl = nw = nc = 0;
+
+    while ((c = getchar()) != EOF)
+    {
+        ++nc;
+        if (c == '\n')
+        {
+            ++nl;
+        }
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT)
+        {
+            state = IN;
+            ++nw;
+        }
+    }
+
+    printf("New lines: %d Words: %d Chars: %d\n", nl, nw, nc);
+    return 0;
+}
+
+//A bug possible is if we type digits not letters and the program will still count it as a world
+/*
+    TEST INPUT:
+    Hello World
+    1234
+    ^Z
+    
+    OUTPUT:
+    New lines: 2 Words: 3 Chars: 18
+    
+*/
