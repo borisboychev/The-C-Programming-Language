7c664970cdf70ea0d7f141fdf367466a79ea9082
BorisBBoychev <borisboychev007@Gmail.com>
2020-08-20 16:46:31 +0300

    exercise 1.12 printint every word on a different line

---
+++ Chapter01/Exercise1.12/wordperline.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define IN 0
+#define OUT 1
+
+int main()
+{
+
+    int c, nw;
+
+    nw = 0;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (!(c == ' '))
+        {
+            putchar(c);
+        }
+        else
+        {
+            putchar('\n');
+        }
+    }
+
+    return 0;
+}
+
+//Exercise 1.12 from page 23
