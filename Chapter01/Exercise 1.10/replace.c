d7c507ca4dbc9ddaa9004752a8bc0cb23413e7a2
BorisBBoychev <borisboychev007@Gmail.com>
2020-08-20 16:27:21 +0300

    names changed with leading 0

---
+++ Chapter01/Exercise 1.10/replace.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+    int c, tab, backspace, slash;
+    tab = backspace = slash = 0;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\t')
+        {
+            if (!tab)
+            {
+                tab = 1;
+                putchar(c);
+            }
+        }
+        else if (c == '\b')
+        {
+            if (!backspace)
+            {
+                backspace = 1;
+                putchar(c);
+            }
+        }
+        else if (c == '\\')
+        {
+            if (!slash)
+            {
+                slash = 1;
+                putchar(c);
+            }
+        }
+        else
+        {
+            backspace = tab = slash = 0;
+            putchar(c);
+        }
+    }
+
+    return 0;
+}
+
+//Exercise 1.10 from page 21
---
+++ Chapter01/Exercise1.09/blank.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int c, blank;
+    blank = 0;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ')
+        {
+            if (blank < 1)
+            {
+                blank = 1;
+                putchar(c);
+            }
+        }
+        else
+        {
+            blank = 0;
+            putchar(c);
+        }
+    }
+
+    return 0;
+}
+
+//Exercise 1.9 from page 21
