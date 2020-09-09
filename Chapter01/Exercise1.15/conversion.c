b5e3b733d83a215d0e984dfb43e7b5acbd2a5f4e
BorisBBoychev <borisboychev007@Gmail.com>
2020-09-09 11:11:02 +0300

    exercise 1.15

---
+++ Chapter01/Exercise1.15/conversion.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int convert(float lower, float upper, float step, float fahr, float celsius);
+int main()
+{
+    printf("---Celsius To Fahrenheit---\n");
+    float fahr, celsius;
+    float lower, upper, step;
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    convert(lower, upper, step, fahr, celsius);
+}
+
+int convert(float lower, float upper, float step, float fahr, float celsius)
+{
+    fahr = lower;
+
+    while (fahr <= upper)
+    {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("\t%.2f\t\t %.2f\n", celsius, fahr);
+        fahr = fahr + step;
+    }
+}
+
+//Exercise 1.15 from page 27
